d50d87f428720f8d3fa4c90bf194cfeffa1ebcb2
Aditi Verma <aditiverma2720@gmail.com>
2026-08-30 23:03:50 +0530

    Add REST controllers exposing Book and Member CRUD endpoints

---
+++ src/main/java/library_managment/Controller/BookController.java
@@ -0,0 +1,42 @@
+package library_managment.Controller;
+
+import library_managment.Model.Book;
+import library_managment.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return "Book deleted with id: " + id;
+    }
+}
---
+++ src/main/java/library_managment/Controller/MemberController.java
@@ -0,0 +1,42 @@
+package library_managment.Controller;
+
+import library_managment.Model.Member;
+import library_managment.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/{id}")
+    public Member getMemberById(@PathVariable Long id) {
+        return memberService.getMemberById(id);
+    }
+
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    @PutMapping("/{id}")
+    public Member updateMember(@PathVariable Long id, @RequestBody Member member) {
+        return memberService.updateMember(id, member);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+        return "Member deleted with id: " + id;
+    }
+}
